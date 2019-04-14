43b2e9bbba2a2ffa9e2371a5251171a985b69be5
Milan Shah <milanshah1@gmail.com>
2019-04-14 18:07:34 +0530

    added full size for carousel

--- application/views/static/home.php
+++ application/views/static/home.php
@@ -1,24 +1,24 @@
-<section>
+<section class="h-75" style="min-height: 100vh;">
   <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img class="d-block w-100" src="<?=base_url();?>/assets/front/img/1.jpg" alt="First slide">
+      <img class="d-block w-100" style="min-height: 100vh;" src="<?=base_url();?>/assets/front/img/1.jpg" alt="First slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="<?=base_url();?>/assets/front/img/2.jpg" alt="Second slide">
+      <img class="d-block w-100" style="min-height: 100vh;" src="<?=base_url();?>/assets/front/img/2.jpg" alt="Second slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="<?=base_url();?>/assets/front/img/3.jpg" alt="Third slide">
+      <img class="d-block w-100" style="min-height: 100vh;" src="<?=base_url();?>/assets/front/img/3.jpg" alt="Third slide">
     </div>
     <div class="carousel-item">
-      <img class="d-block w-100" src="<?=base_url();?>/assets/front/img/4.jpg" alt="Fourth slide">
+      <img class="d-block w-100" style="min-height: 100vh;" src="<?=base_url();?>/assets/front/img/4.jpg" alt="Fourth slide">
     </div>
   </div>
-  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+  <a class="carousel-control-prev" href="#carouselExampleControls" style="min-height: 100vh;" role="button" data-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="sr-only">Previous</span>
   </a>
-  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+  <a class="carousel-control-next" href="#carouselExampleControls" style="min-height: 100vh;" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
   </a>
