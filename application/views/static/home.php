01cf83f0ceaa60a87693152b287e091e69d9d54a
thameemk612 <thameemk612@gmail.com>
2019-04-14 17:11:38 +0530

    carosel

--- application/views/static/home.php
+++ application/views/static/home.php
@@ -1,8 +1,28 @@
 <section>
-  <img src="<?=base_url();?>/assets/front/img/1.jpg"  style="width:100%;height:450px;" class="img-fluid myslides" >
-  <img src="<?=base_url();?>/assets/front/img/2.jpg"  style="width:100%;height:450px;" class="img-fluid myslides" >
-  <img src="<?=base_url();?>/assets/front/img/3.jpg"  style="width:100%;height:450px;" class="img-fluid myslides" >
-  <img src="<?=base_url();?>/assets/front/img/4.jpg"  style="width:100%;height:450px;" class="img-fluid myslides" >
+  <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-100" src="<?=base_url();?>/assets/front/img/1.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="<?=base_url();?>/assets/front/img/2.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="<?=base_url();?>/assets/front/img/3.jpg" alt="Third slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-100" src="<?=base_url();?>/assets/front/img/4.jpg" alt="Fourth slide">
+    </div>
+  </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
 </section>
 <section class="mb-5 mt-5 ">
   <div class="container">
