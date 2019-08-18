8047a2e8dafd675e9cd987ab840bebe146d3ca64
thameemk612 <thameemk612@gmail.com>
2019-08-18 13:19:19 +0530

    footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -34,7 +34,7 @@
             <h5 class="head-font mt-3" style="letter-spacing: .1em;">Counselor</h5>
             Mrs. Reshna S : <a href="tel:+919496410839">+91 9496410839</a>
             <h5 class="head-font mt-3">Webmaster</h5>
-            Thameem K : <a href="tel:+918281582725">+91 8281582725</a>
+            <a href="//thameem.me/">Thameem K </a>
           </p>
         </div>
       </div>
